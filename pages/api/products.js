dd7ca43ae912cfb8896bb65a4b34095e5be9b479
Chinwenma <okoriechinwe1327@gmail.com>
2023-07-18 21:06:29 -0800

    proucts

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,10 +1,21 @@
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
-// import mongoose from "mongoose";
-
 export default async function handle(req, res) {
 const {method} = req; 
 await mongooseConnect();
+
+
+if (method === 'GET') {
+  // if (req.query?.id) {
+    res.json(await Product.find());
+  // } else {
+    // res.json(await Product.find());
+  // }
+}
+
+
+
+
 if (method === 'POST') {
     const {title,description,price} =req.body;
   const productDoc = await Product.create({
--- pages/products.js
+++ pages/products.js
@@ -1,10 +1,36 @@
 import Layout from "@/components/Layout";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Products() {
+const [products,setProducts] =useState([]);
+useEffect(() => {
+  axios.get('/api/products').then(response => {
+  setProducts(response.data);
+});
+}, []);
+
+
   return (
     <Layout>
       <Link href={"/products/new"} className="bg-slate-900 text-white py-2 px-2 rounded-lg shadow-lg">Add New Product</Link>
+      <table className="basic">
+        <thead>
+          <tr>
+            <td>Product Name</td>
+            <td></td>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map(product => (
+            <tr key={product._id}>
+              <td>{product.title}</td>
+              <td>buttons</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </Layout>
   );
 }
