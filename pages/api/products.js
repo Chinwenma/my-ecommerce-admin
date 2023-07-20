94e7ebcfc538caba4cd1e28c85ecbd1cb3e1b1df
Chinwenma <okoriechinwe1327@gmail.com>
2023-07-19 20:50:27 -0800

    editproucts

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,16 +1,20 @@
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
+
+
+
+
 export default async function handle(req, res) {
 const {method} = req; 
 await mongooseConnect();
 
 
 if (method === 'GET') {
-  // if (req.query?.id) {
+  if (req.query?.id) {
+    res.json(await Product.findOne({_id:req.query.id}));
+  } else {
     res.json(await Product.find());
-  // } else {
-    // res.json(await Product.find());
-  // }
+  }
 }
 
 
